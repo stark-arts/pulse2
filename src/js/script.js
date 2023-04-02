a954c8a7c158a6e82abf9583e97d8e320280cea7
starkarts <miinx@yandex.ru>
2023-04-02 06:11:32 +0300

    complete block carousel whith slick slider

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,15 @@ $(document).ready(function(){
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/slide_left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/slide_right.svg"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/slide_right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrow: false
+                }
+            }
+        ]
     });
   });
