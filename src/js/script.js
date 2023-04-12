90d17cb205b54645bdb09ebeb24bebbe4d1a8f0b
starkarts <miinx@yandex.ru>
2023-04-12 07:42:10 +0300

    create new section catalog whith tabs

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/slide_left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/slide_right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slide_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slide_right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
